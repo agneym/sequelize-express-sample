ba73d9502f415333d1f5049e72cd43f3c825b644
Agney <agney.menon@bigbinary.com>
2020-08-02 01:20:27 +0530

    express essentials

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const compression = require("compression");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
 /* Load configuration for dotenv */
 dotenv.config();
 
 const app = express();
+
+app.use(logger("dev"));
+app.use(compression());
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
