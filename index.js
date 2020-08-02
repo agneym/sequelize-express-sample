d154d96d35159be2b3cf9c67ad8ddb1b3906763a
Agney <agney.menon@bigbinary.com>
2020-08-02 10:31:57 +0530

    setup routes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const compression = require("compression");
 const helmet = require("helmet");
-const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
@@ -11,14 +10,31 @@ dotenv.config();
 
 const app = express();
 
+/* Logger */
 app.use(logger("dev"));
+
+/* Add GZIP in express. Use reverse proxy in production */
 app.use(compression());
+
+/* Protect from HTTP vulnerabilities */
 app.use(helmet());
 
-app.use(bodyParser.json());
+/* Arguments are of the form of JSON */
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/", require("./routes"));
+
+app.use(function (err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
